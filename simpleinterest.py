f50f4620ce8fdc082dba4edfb63e1efa487dcdd4
Nikhil Patil <159524719+patilnik1@users.noreply.github.com>
2026-02-27 17:42:39 +0530

    Add files via upload

---
+++ simpleinterest.py
@@ -0,0 +1,8 @@
+#progfram to calculate simple interest
+
+p = float(input("Enter value of p:"))
+r = float(input("Enter value of rate:"))
+n = int(input("Enter number of year:"))
+
+Si = p*r*n/100
+print("Simple Interest = ", Si)
